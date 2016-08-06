a0b7b4e5d94f28e2f83ed13f45bbf8c8f9ede109
Jacqui Ahn <jsahn23@gmail.com>
2016-08-06 00:16:11 -0400

    finishes Ch3 railstutorial.org

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,6 +4,11 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   def setup
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
+  # 
+  # test "should get root" do
+  #   get static_pages_home_url
+  #   assert_response :success
+  # end
 
   test "should get home" do
     get static_pages_home_url
@@ -20,6 +25,6 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "About | #{@base_title}"    
+    assert_select "title", "About | #{@base_title}"
   end
 end
